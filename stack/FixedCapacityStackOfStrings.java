aa292bf0229fdfd2e607121595c0910dbffd503b
youssefelya <youssefelyamani96@gmail.com>
2020-03-20 12:14:51 +0100

    add resize function

--- stack/FixedCapacityStackOfStrings.java
+++ stack/FixedCapacityStackOfStrings.java
@@ -8,7 +8,21 @@ public class FixedCapacityStackOfStrings {
     }
     public boolean isEmpty(){return  N== 0;}
     public void push(String item){
+        if(N== s.length) resize(2*s.length);
         s[N++] = item;
     }
-    public String pop(){return s[--N];}
+    public String pop(){
+        String item = s[--N];
+        s[N] = null;
+        if(N>0 && N==s.length/4) resize(s.length/2); //Array is between 25% and 100% full
+        return item;
+    }
+
+    private void resize(int capacity){
+        String[] copy = new String[capacity];
+        for(int i=0; i<N; i++){
+            copy[i] = s[i];
+        }
+        s=copy;
+    }
 }
