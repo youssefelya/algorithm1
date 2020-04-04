fa8d49819517a7525ac0ee8a0bdc753134213e73
youssefelya <youssefelyamani96@gmail.com>
2020-04-05 00:37:00 +0100

    graph algorithm

--- graph/Digraph.java
+++ graph/Digraph.java
@@ -2,17 +2,19 @@ package graph;
 
 import java.util.LinkedList;
 
-public class Graph {
+public class Digraph {
     private final int V;
     private LinkedList<Integer>[] adj;
-    public Graph(int v) {
+    //Digraph reverse;
+    public Digraph(int v) {
         this.V = v;
         adj = new LinkedList[V];
         for(int i=0; i<V; i++)adj[i] = new LinkedList<>();
+     //   reverse = new Digraph(V);
     }
     public void addEdge(int v, int w){
         adj[v].add(w);
-        adj[w].add(v);
+       // reverse.addEdge(w,v);
     }
 
     public Iterable<Integer> adj(int v){
@@ -21,5 +23,15 @@ public class Graph {
 
     public int V() {
         return V;
+    }
+
+    public Digraph reverse() {
+        Digraph reverse = new Digraph(V);
+        for(int i=0; i<=V; i++){
+            for(int w : adj(i)){
+                reverse.addEdge(w,i);
+            }
+        }
+        return reverse;
     }
 }
